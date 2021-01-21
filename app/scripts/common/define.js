a75eb67a8b66cf0797a82d4ccd0ed38579c19b12
Nhân Trần <tranthainhan1996@gmail.com>
2021-01-21 21:46:25 +0700

    fix conflict

--- app/scripts/common/define.js
+++ app/scripts/common/define.js
@@ -66,7 +66,6 @@ Object.assign(HTMLElement.prototype, {
   },
 });
 
-<<<<<<< HEAD
 //String
 Object.assign(String.prototype, {
   toHTML: function () {
@@ -103,42 +102,13 @@ Object.assign(String.prototype, {
               AT.cart = cart;
               document.dispatchEvent(new CustomEvent("cartChange", { detail: cart }));
             });
-=======
-// (function () {
-//   let nativeFetch = window.fetch;
-//   window.fetch = function (...args) {
-//     let url = args[0];
-//     let promise;
-//     switch (url) {
-//       case "/cart/add.js":
-//         promise = nativeFetch(...args).then(async (response) => {
-//           response
-//             .clone()
-//             .json()
-//             .then((product) => {
-//               nativeFetch(
-//                 `${theme.url.recommendations}?section_id=product-recommendations&product_id=${product.product_id}`
-//               )
-//                 .then((res) => res.text())
-//                 .then((content) => {
-//                   theme.loadCartNotity && document.dispatchEvent(new CustomEvent("cartNotifyDestroy"));
-//                   document.dispatchEvent(new CustomEvent("cartNotify", { detail: { product, recommend: content } }));
-//                 });
-//             });
-//           nativeFetch("/cart.js")
-//             .then((res) => res.json())
-//             .then((cart) => {
-//               AT.cart = cart;
-//               document.dispatchEvent(new CustomEvent("cartChange", { detail: cart }));
-//             });
->>>>>>> ad353a22bd5fc2245046b5fb9e359c7579a03235
 
-//           return response;
-//         });
-//         break;
-//       default:
-//         promise = nativeFetch(...args);
-//     }
-//     return promise;
-//   };
-// })();
+          return response;
+        });
+        break;
+      default:
+        promise = nativeFetch(...args);
+    }
+    return promise;
+  };
+})();
